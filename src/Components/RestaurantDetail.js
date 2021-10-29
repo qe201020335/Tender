3564bca080d60bc1bbacc332eb8f8f25521d7d8c
Sky Z <qe201020335@sina.com>
2021-10-29 03:30:25 -0400

    fix details card

--- src/Components/RestaurantDetail.js
+++ src/Components/RestaurantDetail.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import SwipeButtonsBar from "./SwipeButtonsBar";
 import "./RestaurantDetail.css"
 
 const RestaurantDetail = ({ restaurant }) => {
@@ -8,7 +7,7 @@ const RestaurantDetail = ({ restaurant }) => {
       <div className="restDetailContainer">
         <div className="title"><h1>Mcdonald's</h1></div>
         <div className="address"><h3>675 Yonge St, Toronto, ON M4Y 1T2</h3></div>
-        <div className="body"><p>McDonald's is an American fast food company,
+        <div className="card_body"><p>McDonald's is an American fast food company,
          founded in 1940 as a restaurant operated by Richard and Maurice McDonald, 
          in San Bernardino, California, United States.</p></div>
       </div>
