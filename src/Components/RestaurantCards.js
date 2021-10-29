65295b8bc9747e5072f30562f7b735b799f5492e
Sky Z <qe201020335@sina.com>
2021-10-29 03:49:14 -0400

    forget to change a name

--- src/Components/RestaurantCards.js
+++ src/Components/RestaurantCards.js
@@ -5,22 +5,22 @@ import Card from "./Card";
 //import { useEffect } from 'react'
 
 const RestaurantCards = ({ onCardClick }) => {
-  const [restaurants, setRestaurant] = useState([
+  const [restaurants, setRestaurants] = useState([
     // Hardcoded for phase 1
     { name: "Popeyes",
       image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F20%2F2020%2F06%2F09%2Fpopeyes.jpg&q=85",
       address: "553 Bloor St W, Toronto, ON M5S 1Y6",
-      description: "McDonald's is an American fast food company, founded in 1940 as a restaurant operated by Richard and Maurice McDonald, in San Bernardino, California, United States."
+      description: "Popeyes description."
     },
     { name: "Mcdonald's",
       image: "https://www.eatthis.com/wp-content/uploads/sites/4/2021/06/mcdonalds-tray.jpg",
       address: "675 Yonge St, Toronto, ON M4Y 1T2",
-      description: "Not yet 1"
+      description: "McDonald's is an American fast food company, founded in 1940 as a restaurant operated by Richard and Maurice McDonald, in San Bernardino, California, United States."
     },
     { name: "Subway",
       image: "https://www.nrn.com/sites/nrn.com/files/Subway-Melts-Franchisee-Alert.jpg",
       address: "917 Bay St., Toronto, ON M5S 1Z9",
-      description: "Not yet 2"
+      description: "Subway description."
     }
     ]);
 
