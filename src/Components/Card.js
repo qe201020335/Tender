21add9b5432eb194e5435b719631c21333f8aff2
Sky Z <qe201020335@sina.com>
2021-10-29 04:20:04 -0400

    no more click and drag conflict

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,8 +3,29 @@ import "./RestaurantCards.css";
 import TinderCard from "react-tinder-card";
 
 const Card = ({ restaurant, onCardClick, onCardSwipe, onCardLeft }) => {
+
+  let downx;
+  let downy;
+  let upx;
+  let upy;
+
+  const onMouseDown = (e) => {
+    downx = e.clientX
+    downy = e.clientY
+  }
+
+  const onMouseUp = (e) => {
+    upx = e.clientX
+    upy = e.clientY
+
+    const distance = Math.sqrt((downy - upy)**2 + (downx - upx)**2)
+    if (distance < 10) {
+      onCardClick(restaurant)
+    }
+  }
+
   return (
-    <div onClick={() => onCardClick(restaurant)} className="swipe">
+    <div onMouseUp={onMouseUp} onMouseDown={onMouseDown} className="swipe">
       <TinderCard
         key={restaurant.name}
         preventSwipe={["up","down"]}
