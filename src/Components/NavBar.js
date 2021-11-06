2bff23cf14f63f90f851d246d8a6afdc48bc852a
Sky Z <qe201020335@sina.com>
2021-11-06 19:28:57 -0400

    navbar changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,25 +6,33 @@ import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import { IconButton } from '@material-ui/core';
 import StorefrontIcon from '@material-ui/icons/Storefront';
 import StarIcon from '@material-ui/icons/Star';
-import logo from "../Images/tender_rec.png";
+import logo from "../Images/tender_sq.png";
 
 const NavBar = () => {
   return (
     <div className='header'>
-      <div className='header_left'>
-        <IconButton component={Link} to="/Profile"><AccountBoxIcon frontSize="large" className="header_icon"/>
+      <div id='header_left'>
+
+        <IconButton component={Link} to="/">
+          <img className="header_icon, header_logo" src={logo} alt="TENDER"/>
         </IconButton>
+
         <IconButton component={Link} to="/fav">
           <StarIcon frontSize="large" className="header_icon"/>
         </IconButton>
+
       </div>
-      <IconButton component={Link} to="/">
-        <img className="header_logo" src={logo} alt="TENDER"/>
-      </IconButton>
-      <div className='header_right'>
+
+
+      <div id='header_right'>
+
         <IconButton component={Link} to="/restaurant">
           <StorefrontIcon frontSize="large" className="header_icon"/>
         </IconButton>
+
+        <IconButton component={Link} to="/Profile"><AccountBoxIcon frontSize="large" className="header_icon"/>
+        </IconButton>
+
       </div>
     </div>
   );
