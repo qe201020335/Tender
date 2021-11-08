48f6b370554b105ddc422130b3ca84471efe9875
Yichen <charlestsai0919@gmail.com>
2021-11-07 19:59:03 -0500

    fav page styling

--- src/Components/FavouritesCard.js
+++ src/Components/FavouritesCard.js
@@ -1,14 +1,25 @@
 "use strict";
 import React from 'react';
 import "./FavouritesCard.css";
+import NotInterestedIcon from '@mui/icons-material/NotInterested';
+import { IconButton } from '@material-ui/core';
 
 const FavouritesCard = ({ restaurant, handleUnFavorite }) => {
   return (
     <div className='list_items_container'>
       <li className="list_items" key={restaurant.name}>
-        <b className='restaurant_name'>{restaurant.name}</b><button onClick={() => {handleUnFavorite(restaurant)}}>unfavorite</button><br/>
-        {restaurant.address}
+
+        <img className="fav_images" src={restaurant.image}/>
+        <div className="rest_info">
+          <b className='restaurant_name'>{restaurant.name}</b>
+          <IconButton className='unfav'>
+            <NotInterestedIcon onClick={() => {handleUnFavorite(restaurant)}}> unfavorite </NotInterestedIcon>
+          </IconButton>
+          <br/><br/>
+          <a>{restaurant.address}</a>
+        </div>
       </li>
+      <br/>
       <br/>
     </div>
   );
