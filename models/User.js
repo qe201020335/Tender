968ec3bd376ed849bc0d1d976935f7c79d488273
Ze Jin <ze.jin@mail.utoronto.ca>
2021-11-29 14:47:56 -0500

    added password hashing to model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const CommentSchema = new mongoose.Schema({
     userId: String,
@@ -7,9 +9,15 @@ const CommentSchema = new mongoose.Schema({
 });
 
 const RestaurantSchema = new mongoose.Schema({
-    name: String,
+    name:  {
+        type: String,
+        required: true
+    },
     image: String,
-    address: String,
+    address:  {
+        type: String,
+        required: true
+    },
     description: String,
     comments: [CommentSchema],
     likes: [String],
@@ -17,15 +25,51 @@ const RestaurantSchema = new mongoose.Schema({
 });
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    type: String,
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Invalid Email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4
+    },
+    userType: {
+        type: String,
+        required: true
+    },
     restaurant: RestaurantSchema,
     favourite: [String],
     likes: [String],
     dislikes: [String]
 });
 
+// Mongoose middleware runs prior to saving the document in the database.
+UserSchema.pre('save', function(next) {
+    // binds this to User document instance
+	const user = this; 
+	// checks to ensure we don't hash password more than once
+	if (user.isModified('password')) {
+		// generate salt and hash the password
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash
+				next()
+			})
+		})
+	} else {
+		next()
+	}
+})
+
 const User = mongoose.model('User', UserSchema);
+
 
 module.exports = { User };
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const session = require("express-session");
+// const MongoStore = require('connect-mongo');
 dotenv.config({path: './config/config.env'});
 
 const app = express();
 
+const login = require('./routes/login')
+
+const TEST_USER_ID = '5fb8b011b864666580b4efe3' // the id of our test user (you will have to replace it with a test user that you made). can also put this into a separate configutation file
+
+const TEST_USER_EMAIL = 'test@user.com'
+
 app.use(express.json());
 
-app.use(express.static(__dirname + '/pub'));
+app.use(express.static(__dirname + '/client/build/'));
+
+// enable CORS if in development
+const cors = require('cors')
+if (env !== 'PROD') {
+   app.use(cors())
+}
+
+app.use('/login', login);
 
 app.listen(process.env.PORT, 
   console.log(`Server running in ${process.env.PORT} mode on port ${process.env.PORT}`));
