d18790b591e085c76cdbce108488f1a1e5697d9f
Sky Z <qe201020335@sina.com>
2021-12-05 19:47:17 -0500

    mongoose connect

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
 
 // don't buffer db requests if the db server isn't connected, minimizes http requests hanging if this is the case.
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, bufferCommands: false })
-      .catch((error) => { console.log(error)});
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, bufferCommands: false })
+//       .catch((error) => { console.log(error)});
+const mongoURI = process.env.MONGO_URI
+
+try {
+  mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, bufferCommands: false })
+} catch (error) {
+  console.log(error)
+}
+
 module.exports = { mongoose };
