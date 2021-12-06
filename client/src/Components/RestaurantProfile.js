0840cf93bedfb72063341e4803c7b5439990aebd
Sky Z <qe201020335@sina.com>
2021-12-05 23:28:08 -0500

    react mui store profile

--- client/src/Components/RestaurantProfile.js
+++ client/src/Components/RestaurantProfile.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react'
-import RestaurantProfileView from './RestaurantProfileView'
-import RestaurantProfileForm from './RestaurantProfileForm'
 import "./RestaurantProfile.css";
-import { Redirect } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import {TextField} from "@material-ui/core";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const RestaurantProfile = ({restaurantID, editingState, setEditedRest}) => {
   const [isEditing, setIsEditing] = useState(editingState);
@@ -11,7 +12,13 @@ const RestaurantProfile = ({restaurantID, editingState, setEditedRest}) => {
   useEffect(() => {
     // TODO: get current restaurant from backend
     console.log(restaurantID)
-    setCurrRestaurant(null)
+    setCurrRestaurant({ name: "McDonald's",
+      contact: "+1(416)413-1442",
+      image: "https://www.eatthis.com/wp-content/uploads/sites/4/2021/06/mcdonalds-tray.jpg",
+      address: "675 Yonge St, Toronto, ON M4Y 1T2",
+      description: "McDonald's is an American fast food company, founded in 1940 as a restaurant operated by Richard and Maurice McDonald, in San Bernardino, California, United States."
+    })
+    console.log(currRestaurant)
   }, [])
 
   const onEditClick = () => {
@@ -37,17 +44,73 @@ const RestaurantProfile = ({restaurantID, editingState, setEditedRest}) => {
     }));
   }
 
-  return(
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#0971f1',
+        darker: '#053e85',
+      },
+      tender: {
+        main: '#ca3b28',
+        contrastText: '#ffffff',
+      },
+    },
+  });
+
+  const ColorTextField = styled(TextField)({
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'black',
+        borderWidth: '2px'
+      },
+      // '&:hover fieldset': {
+      //   borderColor: 'blue',
+      // },
+      '&.Mui-focused fieldset': {
+        borderColor: '#ca3b28',
+      },
+      '&.Mui-disabled': {
+        borderColor: 'black',
+        color: 'black'
+      }
+    },
+  });
+
+  return( currRestaurant &&
     <div>
       <br/> <br/>
       <h2 className='txtheader'>Store Profile</h2>
 
-      { isEditing ? <RestaurantProfileForm restaurant={currRestaurant} onRestaurantEdit={onRestaurantEdit}/>
-        : <RestaurantProfileView restaurant={currRestaurant}/> }
+      <div className='restaurantInfo_container'>
+
+        <ThemeProvider theme={theme}>
+          <TextField className="restaurant_input" type="text" placeholder="Enter Name" disabled={!isEditing}
+                     name="name" label="Name" variant="outlined" value={currRestaurant.name}
+                     onChange={onRestaurantEdit} margin="normal"/>
+
+          <TextField className="restaurant_input" type="text" placeholder="Enter Contact Info" disabled={!isEditing}
+                     name="contact" label="Contact" variant="outlined" value={currRestaurant.contact}
+                     onChange={onRestaurantEdit} margin="normal"/>
+
+          <TextField className="restaurant_input" type="text" placeholder="Enter Address" disabled={!isEditing}
+                     name="address" label="Address" variant="outlined" value={currRestaurant.address}
+                     onChange={onRestaurantEdit} margin="normal"/>
+
+          <TextField className="restaurant_input" type="text" placeholder="Enter Description" disabled={!isEditing}
+                     name="description" label="Description" variant="outlined" value={currRestaurant.description}
+                     onChange={onRestaurantEdit} margin="normal" minRows="6" multiline/>
+        </ThemeProvider>
+      </div>
+
+
 
       <div className='button_container'>
-        { isEditing ? <button type="submit" onClick={onEditSubmit}>Save Changes</button>
-          : <button type="submit" onClick={onEditClick}>Edit</button> }
+        <ThemeProvider theme={theme}>
+          <Button type="submit" onClick={isEditing ? onEditSubmit : onEditClick}
+                  variant="contained" color="tender" fullWidth>
+            {isEditing ? "Save Changes!" : "Edit"}
+          </Button>
+        </ThemeProvider>
       </div>
 
       <div className="restaurant_pic">
