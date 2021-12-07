fd849a33ffc24edcddec049f856083898a5a1361
Sky Z <qe201020335@sina.com>
2021-12-07 02:27:00 -0500

    more default rest image and info

--- client/src/Components/AdminRestListEntry.js
+++ client/src/Components/AdminRestListEntry.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {IconButton} from "@material-ui/core";
 import EditIcon from "@mui/icons-material/EditRounded";
 import "./AdminRestListEntry.css"
+import logo_rec from "../Images/tender_rec.png";
 
 const AdminRestListEntry = ( {restaurant, onClickEdit} ) => {
 
@@ -12,14 +13,14 @@ const AdminRestListEntry = ( {restaurant, onClickEdit} ) => {
   return (
     <div className="li_container">
       <li className="AdminRestEntry">
-        <img className="rest_image" src={restaurant.image} alt="restaurant"/>
+        <img className="rest_image" src={!restaurant.image ? logo_rec : restaurant.image} alt="restaurant"/>
         <div className="rest_info">
-          <b className='rest_name'>{restaurant.name}</b>
+          <b className='rest_name'>{restaurant.name ?? `Restaurant ${restaurant._id}`}</b>
           <IconButton className='edit_button'>
             <EditIcon onClick={onClickEditButton}> Edit </EditIcon>
           </IconButton><br/>
-          <span className="rest_info_span">{restaurant.phoneNumber}</span><br/>
-          <span className="rest_info_span">{restaurant.address}</span>
+          <span className="rest_info_span">{restaurant.phoneNumber ?? "Phone"}</span><br/>
+          <span className="rest_info_span">{restaurant.address ?? "Address"}</span>
         </div>
       </li>
       <br/>
--- client/src/Components/FavouritesCard.js
+++ client/src/Components/FavouritesCard.js
@@ -2,20 +2,21 @@ import React from 'react';
 import "./FavouritesCard.css";
 import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
 import { IconButton } from '@material-ui/core';
+import logo_rec from "../Images/tender_rec.png";
 
 const FavouritesCard = ({ restaurant, handleUnFavorite }) => {
   return (
     <div className='list_items_container'>
-      <li className="list_items" key={restaurant.name}>
+      <li className="list_items" key={restaurant._id}>
 
-        <img className="fav_images" src={restaurant.image} alt="restaurant"/>
+        <img className="fav_images" src={!restaurant.image ? logo_rec : restaurant.image} alt="restaurant"/>
         <div className="rest_info">
-          <b className='restaurant_name'>{restaurant.name}</b>
-          <IconButton className='unfav'>
-            <DeleteTwoToneIcon onClick={() => {handleUnFavorite(restaurant)}}> unfavorite </DeleteTwoToneIcon>
+          <b className='restaurant_name'>{restaurant.name ?? `Restaurant ${restaurant._id}`}</b>
+          <IconButton className='unfav' onClick={() => {handleUnFavorite(restaurant)}}>
+            <DeleteTwoToneIcon> unfavorite </DeleteTwoToneIcon>
           </IconButton>
-          <br/><span className="rest_info_span">{restaurant.phoneNumber}</span><br/>
-          <span className="rest_info_span">{restaurant.address}</span>
+          <br/><span className="rest_info_span">{restaurant.phoneNumber ?? "Phone"}</span><br/>
+          <span className="rest_info_span">{restaurant.address ?? "Address"}</span>
         </div>
       </li>
       <br/>
